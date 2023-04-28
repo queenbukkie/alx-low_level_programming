ceb7f46be9a434db9030c9b2896e4ed4e9f95a09
queenbukkie <akinwaleliz00@gmail.com>
2023-04-28 08:04:51 -0700

    ./8-squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,20 +6,16 @@
  */
 void print_square(int size)
 {
-	if (size <= 0)
-	{
-		_putchar('\n');
-	} else
-	{
 		int i, j;
 
 		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
-				_putchar('#');
+				_putchar(35);
 			}
+			if (i != size-1)
 			_putchar('\n');
 		}
-	}
+		_putchar('\n');
 }
