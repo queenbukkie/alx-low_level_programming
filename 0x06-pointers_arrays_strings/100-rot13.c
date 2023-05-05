c16d54160c8ab356e8f2b063b72c5457aa3f519e
queenbukkie <akinwaleliz00@gmail.com>
2023-05-04 17:47:02 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,8 +15,8 @@ char *rot13(char *s)
 	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 		char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
+		for (i = 0; s[i] ![1] != '\0'; i++)
+		{
 			for (j = 0; j < 52; j++)
 			{
 				if (s[i] == data[j])
