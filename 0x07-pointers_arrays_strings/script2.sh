109295a3c778e74298ef2df627fc3f90e740dce2
queenbukkie <akinwaleliz00@gmail.com>
2023-05-08 19:37:06 -0700

    Shege

---
+++ 0x07-pointers_arrays_strings/script2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for passwrd in $(strings ./crackme2)
+do
+ cp crackme2 temp.out
+ bilal=$(./temp.out $passwrd | grep -v "Access denied :(")
+ echo "Trying: $passwrd"
+ if [ "$bilal" != '' ]
+ then
+   printf "\nThe password is: %s\n" "$passwrd"
+   exit 0
+ fi
+done
